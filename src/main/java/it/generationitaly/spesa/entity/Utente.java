bcece9249cc68adc7ef7708dd3769eeabc1c222f
Jeffo <Jeffo@pcJ.lan>
2023-06-27 16:04:04 +0200

    implemented entity Utente.java

--- src/main/java/it/generationitaly/spesa/entity/Utente.java
+++ src/main/java/it/generationitaly/spesa/entity/Utente.java
@@ -1,5 +1,53 @@
 package it.generationitaly.spesa.entity;
 
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+
+@Entity
+@Table(name = "utente")
 public class Utente {
+
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private int id;
+
+	@Column(name = "username", length = 45, unique = true, nullable = false)
+	private String username;
+
+	@Column(name = "password", length = 45, nullable = false)
+	private String password;
+
+	public int getId() {
+		return id;
+	}
+
+	public void setId(int id) {
+		this.id = id;
+	}
+
+	public String getUsername() {
+		return username;
+	}
+
+	public void setUsername(String username) {
+		this.username = username;
+	}
+
+	public String getPassword() {
+		return password;
+	}
+
+	public void setPassword(String password) {
+		this.password = password;
+	}
+
+	@Override
+	public String toString() {
+		return "Utente [id=" + id + ", username=" + username + ", password=" + password + "]";
+	}
 
 }
