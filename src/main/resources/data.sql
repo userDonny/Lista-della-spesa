efd7846f9991de8e07155dec2d6c85d8f93b638b
nicod <nicod@LAPTOP-DMJ7QKIQ.homenet.telecomitalia.it>
2023-06-28 12:37:46 +0200

    Update data.sql and schema.sql

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -1,1 +1,55 @@
-insert into utente (username, password) values ('demo', 'demo');
+INSERT INTO utente VALUES('1','admin','pass');
+INSERT INTO utente VALUES('2','demo','demo');
+INSERT INTO utente VALUES('3','test','test');
+
+
+
+
+insert into etichetta values('1','6.3','57.5','30.9','10.6','0','0.107','539.0');   -- nutella
+insert into etichetta values('2','0.300','14','0','0.200','2.29','0.100','53.0');  -- mele
+insert into etichetta values('3','0.100','0.200','0','0','1.0','0.200','20.0');  -- limoni
+insert into etichetta values('4','0.8','10.0','0.1','0.0','1.2','0.1','48.0');  -- albicocche
+insert into etichetta values('5','1.9','0.0','0.4','0.0','4.0','1.0','48.0');  -- pomodori
+insert into etichetta values('6','0.4','45.0','0.1','0.1','0.8','0.1','188.0'); -- marmellata di frutta  
+insert into etichetta values('7','0.0','0.0','0.0','0.0','0.0','0.350','0.0');  -- acqua 
+insert into etichetta values('8','0.3','11.0','0.1','0.0','0.0','0.8','45.0');  -- red bull
+insert into etichetta values('9','3.30','4.90','0.10','0.070','0.0','0.13','143.0');  -- latte
+insert into etichetta values('9','0.0','29.0','32.0','4.0','0.0','1.5','398.0');  -- formaggio
+insert into etichetta values('10','31.0','0.0','3.6','1.0','0.0','0.074','165.0');  -- petto di pollo
+insert into etichetta values('11','22.0','2.4','26.0','9.0','0.0','0.174','336.0');  -- salame 
+insert into etichetta values('12','20.0','0.0','13.0','3.1','0.0','0.05','208.0'); -- salmone
+insert into etichetta values('13','29.0','0.0','0.6','0.2','0.0','0.054','130.0'); -- tonno
+insert into etichetta values('14','3.9','29.0','6.7','1.5','0.2','2.0','192.0'); -- patatine surgelate
+insert into etichetta values('15','2.8','25.0','10.0','8.7','0.0','0.017','201.0'); -- gelato in vaschetta
+insert into etichetta values('16','0.0','31.0','0.9','0.0','1.8','0.001','158.0');  -- spaghetti
+insert into etichetta values('17','2.0','28.0','0.4','0.1','0.4','0.001','130.0'); -- riso
+insert into etichetta values('18','0.0','0.0','0.0','0.0','0.0','0.0','222.0'); -- jack daniels
+insert into etichetta values('19','0.4','2.8','0.0','0.0','0.0','0.001','120.0'); -- birra moretti
+insert into etichetta values('20','1.0','1.2','35.0','4.0','1.9','1.5','326.0'); -- condimento insalata di riso
+insert into etichetta values('21','2.9','10.8','1.9','0.5','3.8','0.040','80.0'); -- mais in scatola
+
+
+
+insert into categoria values('1','Frutta');
+insert into categoria values('2','Verdura');
+insert into categoria values('3','Acqua e bevande');
+insert into categoria values('4','Latte e derivati');
+insert into categoria values('5','Carne e salumi');
+insert into categoria values('6','Pesce');
+insert into categoria values('7','Surgelati e gelati');
+insert into categoria values('8','Pasta e riso');
+insert into categoria values('9','Vino, birra e liquori');
+insert into categoria values('10','Dolci');
+insert into categoria values('11','Conserve e condimenti');
+
+
+insert into prodotto values('001','Limoni','Monachello','1.98',null,'1.0',null,null,'3','2','1');
+insert into prodotto values('002','Mele Golden','Melinda','2.49',null,'1.0',null,null,'2','1','1');
+insert into prodotto values('003','Nutella','Ferrero','5.50',null,'1.0',null,null,'1','10','1');
+
+
+insert into catena values ('1','Esselunga');
+insert into catena values ('2','Carrefour');
+insert into catena values ('3','Pam');
+insert into catena values ('4','Coop');
+
